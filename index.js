bd3f9486792ceee2afb48bcef49dd6740f775412
Oseias Beu <oseiasbeu@outlook.com>
2018-06-23 16:01:06 -0300

    Testes de requisicoes http com o AXIOS

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios =  require('axios');
+let dados;
+// axios.get("https://api.github.com/users/oseiasbeu").then(function(resposta){
+//   console.log(resposta.data);
+// }).catch(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+
+
+// axios.post("https://jsonplaceholder.typicode.com/posts").then(function(resposta){
+//   console.log(resposta.data);
+// }).catch(function(error){
+//   if(error){
+//     console.log(error);
+//   }
+// });
+
+// axios.post("http://minha.api.io/registrar/usuarios",{email: "meuemail@email.com", senha:"minha senha"});
+
+
+// function usuarioGitHub(){
+//  return axios.get("https://api.github.com/users/oseiasbeu");
+// }
+
+//   dados = usuarioGitHub;
+
+//   dados.then(function(resposta){
+//     console.log(resposta.data);
+//   }).catch(function(error){
+//     if(error){
+//       console.log(error);
+//     }
+//   });
